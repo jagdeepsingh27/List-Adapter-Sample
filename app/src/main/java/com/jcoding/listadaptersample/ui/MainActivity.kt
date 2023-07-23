9429fdba702c6fa3a08460c340766890ab17e479
Jagdeep Singh <132729448+jagdeepsingh27@users.noreply.github.com>
2023-07-23 23:40:15 +0530

    adds swipe delete

--- app/src/main/java/com/jcoding/listadaptersample/ui/MainActivity.kt
+++ app/src/main/java/com/jcoding/listadaptersample/ui/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.jcoding.listadaptersample.ui
 
+import android.content.DialogInterface
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import com.jcoding.listadaptersample.R
 import com.jcoding.listadaptersample.data.model.ListItemData
 import com.jcoding.listadaptersample.databinding.ActivityMainBinding
-import com.jcoding.listadaptersample.ui.ListViewModel
-import com.jcoding.listadaptersample.ui.ListViewModelFactory
 import com.jcoding.listadaptersample.ui.list.EndlessRecyclerViewScrollListener
 import com.jcoding.listadaptersample.ui.list.ItemListAdapter
+import com.jcoding.listadaptersample.ui.list.ListItemSwipeCallback
 import com.jcoding.listadaptersample.ui.list.VerticalSpaceRecyclerItemDecoration
 
 
@@ -20,7 +26,7 @@ class MainActivity : AppCompatActivity() {
     }
     private lateinit var binding: ActivityMainBinding
     private lateinit var listAdapter: ItemListAdapter
-    private lateinit var scrollListener : EndlessRecyclerViewScrollListener
+    private lateinit var scrollListener: EndlessRecyclerViewScrollListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -55,6 +61,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
         binding.listRecyclerView.addOnScrollListener(scrollListener)
+        enableSwipeToDeleteAndUndo()
     }
 
 
@@ -66,6 +73,31 @@ class MainActivity : AppCompatActivity() {
         override fun onDeleteOptionClick(item: ListItemData) {
             listViewModel.removeItem(item)
         }
+    }
+
+    private fun enableSwipeToDeleteAndUndo() {
+        val swipeToDeleteCallback: ListItemSwipeCallback = object : ListItemSwipeCallback(this) {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, i: Int) {
+                val position = viewHolder.adapterPosition
+                val alertDialog: AlertDialog = AlertDialog.Builder(this@MainActivity).apply {
+                    setTitle(getString(R.string.title_delete_item_dialog))
+                    setPositiveButton(getString(R.string.label_delete_item)
+                    ) { _, id ->
+                        listViewModel.removeItem(listAdapter.currentList[position])
+                    }
+                    setNegativeButton(getString(R.string.label_cancel_delete_item)
+                    ) { _, id ->
+                        // User cancelled the dialog
+                    }
+                    setOnDismissListener {
+                        listAdapter.notifyItemChanged(position)
+                    }
+                }.create()
+                alertDialog.show()
+            }
+        }
+        val itemTouchhelper = ItemTouchHelper(swipeToDeleteCallback)
+        itemTouchhelper.attachToRecyclerView(binding.listRecyclerView)
     }
 
 }
